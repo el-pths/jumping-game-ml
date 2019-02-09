58f72183c242732f0e5aa9bf83d2143b8542c5cb
El'Xsanori Leo <39676248+Xsanori@users.noreply.github.com>
2019-02-09 21:18:25 +0300

    Fixes #2. Fixes #3. Add actions
    Add physics, actions (collisions, score, jumping keys, final screen)

--- software/JGField.java
+++ software/JGField.java
@@ -1,9 +1,12 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JPanel;
 import javax.swing.Timer;
-
 /**
  * MacleoJG (jumping-game-ml) Simple jumping game which can be controlled by
  * accelerometer via arduino
@@ -11,7 +14,7 @@ import javax.swing.Timer;
  * @author Xsanori
  *
  */
-public class JGField extends JPanel {
+public class JGField extends JPanel implements ActionListener {
 	private final int jmpow = 60;
 	private int ypos = 448;
 	private int xpos;
@@ -26,12 +29,14 @@ public class JGField extends JPanel {
 	public JGField() {
 		setBackground(Color.white);
 		onInit();
+		addKeyListener(new keyTime());
+		setFocusable(true);
 
 	}
 
 	private void onInit() {
-		// framer = new Timer(10, this);
-		//framer.start();
+		framer = new Timer(10, this);
+		framer.start();
 		createJmp();
 
 	}
@@ -52,6 +57,47 @@ public class JGField extends JPanel {
 			g.drawString("YOU LOSE. SCORE: " + Integer.toString(score), 16, 16);
 		}
 
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+        if (rnng) {
+            if (isjmp) {
+                ypos -= ux * 0.1;
+                ux -= 1;
+            }
+            if (ypos == 448) {
+                isjmp = false;
+            }
+            if (xpos != 0) {
+                xpos -= 5;
+            } else {
+                createJmp();
+            }
+            if (xpos >= 352 && xpos <= 516) {
+                if (ypos >= 348) {
+                    rnng = false;
+                } else if (flag) {
+                    score++;
+                    flag = false;
+                }
+            } else {
+                flag = true;
+            }
+        }
+        repaint();
+    }
+
+	class keyTime extends KeyAdapter {
+		@Override
+        public void keyPressed(KeyEvent e) {
+            super.keyPressed(e);
+            kkd = e.getKeyCode();
+            if (kkd == KeyEvent.VK_SPACE && ! isjmp) {
+                isjmp = true;
+                ux = jmpow;
+            }
+        }
 	}
 
 }
